35de7ec885c40857130a86ed56570c0d64cb3b46
tahsinAmin <muhammadtahsinamin@gmail.com>
2023-04-21 05:37:12 +0600

    Writing data in json file

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require('fs');
 
 let url =
   "https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc";
-// https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc&page=2
-let states = [];
+
+var ads = {
+  table: []
+};
+
 let urlUpdated = false
 
+const finishFunction = () => {
+  console.log("Task Complete");
+}
+
 const getNextPageUrl = (givenUrl, pageNum) => {
-    console.log("Pagenum =", )
     let concatString = `&page=${pageNum}`;
     if (urlUpdated) {
         return givenUrl.substring(0, givenUrl.indexOf("&page=")) + concatString
@@ -21,39 +28,25 @@ const getNextPageUrl = (givenUrl, pageNum) => {
 
 const fetch_data = async () => {
   try {
-    // let res = await axios.get(url);
-    // let $ = await cheerio.load(res.data);
-
-    // GETS THE ITEMS' TITLE ONLY AS ARRAY
-    // $(
-    //     "div.ooa-1nihvj5.eayvfn615 > h2 > a"
-    // ).each((i, e) => {
-    //     states.push($(e).text().trim());
-    // });
-
-    // CONSOLE LOGS THE LAST NUMBER OF THE PAGINATION
-    // let lastPageNumber = $(
-    //     "li.pagination-item > a > span"
-    // ).eq(-1).text().trim()
-
-    // console.log("=> lastPageNumber =", lastPageNumber)
-
-    // GIVING A DUMMY VALUE JUST TO SEE IF IT WORKS.
-    let lastPageNumber = 3;
-    console.log("=> url =", url)
+    let lastPageNumber = 2;
+    
     for (let i = 1; i <= lastPageNumber; i++) {
-        // let res = await axios.get(url);
-        // let $ = await cheerio.load(res.data);
+        let res = await axios.get(url);
+        let $ = await cheerio.load(res.data);
 
-        // $("div.ooa-1nihvj5.eayvfn615 > h2 > a").each((i, e) => {
-        //     states.push($(e).text().trim());
-        // });
+        $("div.ooa-1nihvj5.eayvfn615 > h2 > a").each((i, e) => {
+            let title = $(e).text().trim();
+            ads.table.push({title});
+        });
+        console.log("=> url =", url)
         url = getNextPageUrl(url, i+1)
     }
+
+    fs.writeFile('adsJSON.json', JSON.stringify(ads), 'utf8', finishFunction);
+
   } catch (e) {
     console.log(e);
   }
-  console.log("Finish");
 };
 
 fetch_data();
@@ -61,8 +54,5 @@ fetch_data();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/states", (req, res) => {
-  res.send(states);
-});
 
 app.listen(port, () => console.log("Server Running"));
