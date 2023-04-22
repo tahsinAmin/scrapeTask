dff37722b7ea10d15fdd237e2576ca9ed5f618ff
tahsinAmin <muhammadtahsinamin@gmail.com>
2023-04-22 14:42:51 +0600

    JS array to csv

--- NotImportant/take-2.js
+++ NotImportant/take-2.js
@@ -1,15 +1,31 @@
 const fs = require("fs");
-fs.readFile("./take-2.json", "utf8", (err, jsonString) => {
-  if (err) {
-    console.log("File read failed:", err);
-    return;
-  }
-  const truck = JSON.parse(jsonString);
-  let advertSearch = truck["props"]["pageProps"]["urqlState"]["42099054604"]["data"]["advertSearch"];
-  let pageSize = advertSearch["pageInfo"]["pageSize"];
-  let edges = advertSearch["edges"];
-  console.log(pageSize);    
-  console.log();
-  console.log(edges);    
+const Json2Parser = require("json2csv").Parser;
 
-});
+let trucksData = [];
+for (let h = 0; h < 2; h++) {
+  fs.readFile("./take-2.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }
+    
+    const truck = JSON.parse(jsonString);
+    
+    let advertSearch =
+      truck["props"]["pageProps"]["urqlState"]["42099054604"]["data"][
+        "advertSearch"
+      ];
+    let pageSize = advertSearch["pageInfo"]["pageSize"];
+    let edges = advertSearch["edges"];
+    
+    for (let i = 0; i < pageSize; i++) {
+      let id = edges[i]["node"]["id"];
+      let title = edges[i]["node"]["title"];
+      
+      trucksData.push({ id, title });
+    }
+    const json2csvParser = new Json2Parser();
+    const csv = json2csvParser.parse(trucksData);
+    fs.writeFileSync(`${__filename}.csv`, csv, "utf-8");
+  });
+}
