6da8a4adf6d7a6fe827d930219f8e1087f089e36
tahsinAmin <muhammadtahsinamin@gmail.com>
2023-04-23 21:02:03 +0600

    # Feature - README file updated - scrapeTruckItem function  added

--- README.md
+++ README.md
@@ -10,12 +10,47 @@ Second, run the development server:
 
 ## Tasks
 
-- [] https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc
+- [x] https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc
 - [x] either puppeteer/playwright or request-promise for fetching ads Purpose:
 - [x] Add getNextPageUrl function to iterate over pages
   - [x] A function that returns url string for iterate over pages
 - [] Add addItems function that fetches item urls + item ids (unique ids that the portal uses) from list page
-- [] Add getTotalAdsCount function - shows how many total ads exist for the provided initial url
-- [] Add scrapeTruckItem function - that scrapes the actual ads and parses into the format: item id, title, price, registration date, production date, mileage, power
-  - [x] Writing data in json file
+- [x] Add getTotalAdsCount function - shows how many total ads exist for the provided initial url
+- [x] Add scrapeTruckItem function - that scrapes the actual ads and parses into the format: item id, title, price, registration date, production date, mileage, power
+  - [x] Writing data in CSV file
 - [] BONUS: scraping via otomoto mobile app.
+
+## Questions/thoughts:
+
+### Ideas for error catching/solving, retry strategies?
+
+```
+- Questions
+
+  - For the bonus task, may I get additional time like a day?
+  - Do I need to add Command Line Arguments feature for the functions, "scrapeTruckItem" & "addItems"?
+
+- Ideas for:
+
+  - error catching/solving
+    - Using of debugging tool.
+
+  - retry strategies
+    - Break the data down to more smaller chunk size to analyze.
+    - For script json data, json file created (for the given url) for crosschecking with js object
+```
+
+### Accessing more ads from this link than the limit allows (max 50 pages)?
+
+### Experience with CI/CD tools?
+
+```
+I have experience using Selenium with Python.
+```
+
+### Other considerations?
+
+```
+- getTotalAdsCount function
+  - I have been working with total ads for every page regardless. But since the requirements asked for the initial page only, I created a variable to save the value for the first url only and returning the value.
+```
--- index.js
+++ index.js
@@ -1,58 +1,139 @@
-const express = require("express");
-const axios = require("axios");
+const request = require("request-promise");
 const cheerio = require("cheerio");
-const fs = require('fs');
+const fs = require("fs");
+const { parse } = require("json2csv");
+
+const filePath = "./scrapeTruckItem.csv";
 
 let url =
   "https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc";
 
-var ads = {
-  table: []
-};
-
-let urlUpdated = false
-
-const finishFunction = () => {
-  console.log("Task Complete");
-}
+let urlUpdated = false;
+let initalUrlAdsCount = 0;
 
 const getNextPageUrl = (givenUrl, pageNum) => {
-    let concatString = `&page=${pageNum}`;
-    if (urlUpdated) {
-        return givenUrl.substring(0, givenUrl.indexOf("&page=")) + concatString
+  let concatString = `&page=${pageNum}`;
+  if (urlUpdated) {
+    return givenUrl.substring(0, givenUrl.indexOf("&page=")) + concatString;
+  }
+  givenUrl += concatString;
+  urlUpdated = true;
+  return givenUrl;
+};
+
+const scrapeTruckItem = (edgesArray, noOfAds) => {
+  const adsData = [];
+  for (let j = 0; j < noOfAds; j++) {
+    let registrationDate = edgesArray[j]["vas"]["bumpDate"]; // TODO REfine the Data
+    let node = edgesArray[j]["node"];
+    let id = node["id"];
+    let title = node["title"];
+    let price = node["price"]["amount"]["units"];
+
+    let productionDate = "",
+      mileage = "",
+      power = "";
+    let parametersArr = node["parameters"];
+
+    for (let k = 0; k < parametersArr.length; k++) {
+      let paramItem = parametersArr[k];
+      if (paramItem["key"] === "mileage") {
+        mileage = paramItem["displayValue"];
+      }
+      if (paramItem["key"] === "power") {
+        power = paramItem["value"];
+      }
+      if (paramItem["key"] === "year") {
+        productionDate = paramItem["value"];
+      }
     }
-    givenUrl += concatString
-    urlUpdated = true
-    return givenUrl
-}
+
+    adsData.push({
+      registrationDate,
+      id,
+      title,
+      price,
+      productionDate,
+      mileage,
+      power,
+    });
+  }
+
+  return adsData;
+};
+
+// TODO: Make a helper function ExportToCSV
+const exportToCsvFile = (fPath, data) => {
+  if (fs.existsSync(fPath)) {
+    console.log("File exist");
+    let csv = parse(data, { header: false });
+    fs.appendFileSync(fPath, "\r\n");
+    fs.appendFile(fPath, csv, (err) => {
+      if (err) console.error("Couldn't append the data");
+      console.log("The data was appended to file!");
+    });
+  } else {
+    let csv = parse(data, { header: true });
+    console.log("File Doesn't exist");
+    fs.writeFileSync(fPath, csv, "utf-8");
+  }
+};
+
+const getTotalAdsCount = () => {
+  return initalUrlAdsCount;
+};
 
 const fetch_data = async () => {
   try {
-    let lastPageNumber = 2;
-    
-    for (let i = 1; i <= lastPageNumber; i++) {
-        let res = await axios.get(url);
-        let $ = await cheerio.load(res.data);
+    let i = 1, maxPages = 1;
+    do {
+      console.log("=> url =", url);
+      const response = await request({
+        url,
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+        },
+      });
+      let $ = cheerio.load(response);
 
-        $("div.ooa-1nihvj5.eayvfn615 > h2 > a").each((i, e) => {
-            let title = $(e).text().trim();
-            ads.table.push({title});
-        });
-        console.log("=> url =", url)
-        url = getNextPageUrl(url, i+1)
-    }
+      let jsonData = JSON.parse($("script#__NEXT_DATA__").text());
 
-    fs.writeFile('adsJSON.json', JSON.stringify(ads), 'utf8', finishFunction);
+      let urqlState = jsonData["props"]["pageProps"]["urqlState"];
 
+      let data = "";
+      for (var prop in urqlState) {
+        if (prop === "-2580735814") {
+          continue;
+        }
+        data = urqlState[prop]["data"];
+      }
+      let advertSearch = JSON.parse(data)["advertSearch"];
+      let pageSize = advertSearch["pageInfo"]["pageSize"];
+
+      if (i == 1) {
+        // Finding out maximum number of pages
+        initalUrlAdsCount = pageSize;
+
+        maxPages = parseInt(
+          $("li.pagination-item > a > span").eq(-1).text().trim()
+        );
+        console.log("=> Total Ads in Initial URL =", getTotalAdsCount());
+        console.log("=> maxPages =", maxPages);
+      }
+
+      let edges = advertSearch["edges"];
+      let ads = scrapeTruckItem(edges, pageSize);
+
+      exportToCsvFile(filePath, ads);
+
+      url = getNextPageUrl(url, i + 1); // TODO: Refactor code
+
+      i++;
+    } while (i <= maxPages);
   } catch (e) {
     console.log(e);
   }
 };
 
 fetch_data();
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-
-app.listen(port, () => console.log("Server Running"));
